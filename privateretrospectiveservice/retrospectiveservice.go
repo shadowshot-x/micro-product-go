94a082be0ac91cb18c47f93175868f79187efc4d
ujjwal sharma <ujjwal26599@gmail.com>
2021-12-29 22:27:28 +0530

    mutex logic added

--- privateretrospectiveservice/retrospectiveservice.go
+++ privateretrospectiveservice/retrospectiveservice.go
@@ -1,27 +1,98 @@
 package couponservice
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/go-redis/redis"
+	"sync"
+
 	"go.uber.org/zap"
 	"gopkg.in/olahol/melody.v1"
 )
 
+// we will control this with mutex
+var retrospective string
+
 // RetrospectiveController is the Real Time Socket route handler
 type RetrospectiveController struct {
-	logger *zap.Logger
+	logger         *zap.Logger
+	melodyInstance *melody.Melody
+	access         sync.Mutex
+	currentUser    string
+	status         bool
 }
 
 // NewRetrospectiveController returns a fresh Retrospective controller
-func NewRetrospectiveController(logger *zap.Logger, instance *redis.Client) *RetrospectiveController {
+func NewRetrospectiveController(logger *zap.Logger, melodyInstance *melody.Melody) *RetrospectiveController {
 	return &RetrospectiveController{
-		logger: logger,
+		logger:         logger,
+		melodyInstance: melodyInstance,
+		status:         false,
 	}
 }
 
+// Assigns the Websocket to the request.
 func (ctrl *RetrospectiveController) AssignSocket(rw http.ResponseWriter, r *http.Request) {
-	m := melody.New()
-	fmt.Print(m)
+	ctrl.melodyInstance.HandleRequest(rw, r)
+}
+
+func (ctrl *RetrospectiveController) BroadcastMessage(rw http.ResponseWriter, r *http.Request) {
+	// lets broadcast to everyone
+	ctrl.melodyInstance.Broadcast([]byte(retrospective))
+}
+
+func (ctrl *RetrospectiveController) CheckAccess(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+	if !ctrl.status {
+		rw.Write([]byte("false"))
+	} else {
+		rw.Write([]byte("true"))
+	}
+}
+
+func (ctrl *RetrospectiveController) AvailRetrospective(rw http.ResponseWriter, r *http.Request) {
+	ctrl.status = true
+	ctrl.access.Lock()
+	ctrl.currentUser = r.Header["Username"][0]
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("Access to Retrospective Granted"))
+}
+
+func (ctrl *RetrospectiveController) ChangeRetrospective(rw http.ResponseWriter, r *http.Request) {
+	if ctrl.currentUser == r.Header["Username"][0] && ctrl.status {
+		retrospective = r.Header["Retrospective"][0]
+		ctrl.logger.Info("Restrospective updated by user", zap.String("user", ctrl.currentUser))
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("Retrospective Updated"))
+		// we will also add the log file here
+	} else if !ctrl.status {
+		ctrl.logger.Warn("User tried to access resource without locking the resource")
+		rw.WriteHeader(http.StatusBadRequest)
+		// there should be an alert here
+	} else {
+		rw.WriteHeader(http.StatusConflict)
+		rw.Write([]byte("Access Denied as resource is in use"))
+		ctrl.logger.Warn("User tried to access while resource was locked")
+		// there should be an alert here
+	}
+}
+
+func (ctrl *RetrospectiveController) ReleaseRetrospective(rw http.ResponseWriter, r *http.Request) {
+	// only the current user should be able to call this.
+	if ctrl.currentUser == r.Header["Username"][0] && ctrl.status {
+		ctrl.access.Unlock()
+		ctrl.status = false
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("Retrospective Released"))
+	} else if !ctrl.status {
+		ctrl.logger.Warn("User tried to release resource without locking the resource")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("Retrospective Updated"))
+		// there should be an alert here
+	} else {
+		rw.WriteHeader(http.StatusConflict)
+		rw.Write([]byte("Access Denied as no auth"))
+		ctrl.logger.Warn("Unauthorized User tried to release")
+		// there should be an alert here
+	}
 }
